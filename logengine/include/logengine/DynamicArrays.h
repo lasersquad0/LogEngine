e49cd7eb262a8a7e84fd36e9210a2335eacb8edb
lasersquad0 <lasersquad@gmail.com>
2024-01-28 20:27:50 +0300

    .

--- logengine/include/logengine/DynamicArrays.h
+++ logengine/include/logengine/DynamicArrays.h
@@ -1,9 +1,9 @@
 #ifndef DYNAMIC_ARRAYS_H
 #define DYNAMIC_ARRAYS_H
 
-#ifndef WIN32 // assume it is Linux
-#include <string.h>
-#endif
+//#ifndef WIN32 // assume it is Linux
+//#include <string.h>
+//#endif
 
 #include <exception> 
 #include <string>
